34d99aa5acc1d65fd7cc3fd99c06c3a20e8e283e
Abdu0402 <124436273+Abdu0402@users.noreply.github.com>
2023-02-25 19:35:50 +0600

    Update Main.java

--- problem-B/Main.java
+++ problem-B/Main.java
@@ -1,13 +1,1 @@
-public class Main {
-    public static void main(String[] args) {
-        
 
-
-        Animal a = new Fish();
-        // a.setName("Java");
-
-
-        Animal e = new Spider(); 
-        Pet p = new Cat();
-    }
-}
